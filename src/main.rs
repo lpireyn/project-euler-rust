1493d5f99537de92f4c7f6a5432d6df277421fcf
Laurent Pireyn <laurent@pireyn.be>
2025-09-13 11:11:23 +0200

    feat: Solution to problem 1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-const SOLUTIONS: &[fn()] = &[];
+#[rustfmt::skip]
+const SOLUTIONS: &[fn()] = &[
+    solution1,
+];
 
 fn main() {
     let args = std::env::args().collect::<Vec<_>>();
@@ -12,7 +15,19 @@ fn main() {
     solution();
 }
 
+fn solution1() {
+    let sum = (0..1000)
+        .filter(|n| is_multiple(*n, 3) || is_multiple(*n, 5))
+        .sum::<i32>();
+    println!("{sum}");
+    assert_eq!(sum, 233168);
+}
+
 #[allow(unused)]
 fn unimplemented() {
     panic!("Solution not yet implemented");
+}
+
+fn is_multiple(n: i32, d: i32) -> bool {
+    n % d == 0
 }
